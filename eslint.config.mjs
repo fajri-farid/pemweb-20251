621a2462d7f7d8db32ec1db5b6bb0de5b4308c30
fajri-fard <fajrijry14@gmail.com>
2025-09-10 08:23:26 +0800

    chore(eslint): update config with import plugin and rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,32 @@ import js from '@eslint/js';
 import globals from 'globals';
 import json from '@eslint/json';
 import { defineConfig } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    plugins: { import: importPlugin },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
     rules: {
+      ...js.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules,
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
       'no-unused-vars': 'warn',
+      'import/no-unresolved': ['error', { ignore: ['^eslint/'] }],
+      'import/named': 'error',
+      'import/order': ['warn', { 'newlines-between': 'always' }],
     },
   },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
   {
     files: ['**/*.json'],
     plugins: { json },
     language: 'json/json',
-    extends: ['json/recommended'],
+    rules: {
+      ...json.configs.recommended.rules,
+    },
   },
 ]);
